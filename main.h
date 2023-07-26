07631758c66c7b137a1545f0385bcf2591ff2d1d
JuliusDgenius <ibejulius1@gmail.com>
2023-07-26 12:58:15 +0100

    revert last commits

--- main.h
+++ main.h
@@ -5,8 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-void execmd(char **argv);
-char *get_location(char *command);
+void execute_cmd(char **argv);
+char *get_path(char *cmd);
 
 #endif
