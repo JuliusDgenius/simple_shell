1e49a01bfbe7e1fda76985b6987dac11245fce39
JuliusDgenius <ibejulius1@gmail.com>
2023-07-28 07:18:28 +0100

    parse command

--- tokenize.c
+++ tokenize.c
@@ -2,38 +2,38 @@
 
 
 /**
- * tokenize - tokenizes a buffer with a delimiter
- * @buffer: buffer to tokenize
- * @delimiter: delimiter to tokenize along
+ * tokenize - parses a buffer and tokenize its strings
+ * @buffer: buffer holding the strings
+ * @delimiter: The delimiter to tokenize at
  *
- * Return: pointer to an array of pointers to the tokens
+ * Return: returns a pointer to the tokenized strings.
  */
 char **tokenize(char *buffer, char *delimiter)
 {
-char **tokens = NULL;
-size_t i = 0, mcount = 10;
+	char **tokens = NULL;
+	size_t i = 0, mcount = 10;
 
-if (buffer == NULL)
-return (NULL);
-tokens = malloc(sizeof(char *) * mcount);
-if (tokens == NULL)
-{
-perror("Fatal Error");
-return (NULL);
-}
-while ((tokens[i] = new_strtok(buffer, delimiter)) != NULL)
-{
-i++;
-if (i == mcount)
-{
-tokens = _realloc(tokens, &mcount);
-if (tokens == NULL)
-{
-perror("Fatal Error");
-return (NULL);
-																					}
-}
-buffer = NULL;
-}
-return (tokens);
+	if (buffer == NULL)
+		return (NULL);
+	tokens = malloc(sizeof(char *) * mcount);
+	if (tokens == NULL)
+	{
+		perror("Fatal Error");
+		return (NULL);
+	}
+	while ((tokens[i] = new_strtok(buffer, delimiter)) != NULL)
+	{
+		i++;
+		if (i == mcount)
+		{
+			tokens = _realloc(tokens, &mcount);
+			if (tokens == NULL)
+			{
+				perror("Fatal Error");
+				return (NULL);
+			}
+		}
+		buffer = NULL;
+	}
+	return (tokens);
 }
