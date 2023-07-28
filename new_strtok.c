375373ca410fd0f55a7563f73e56b3452fb4e087
JuliusDgenius <ibejulius1@gmail.com>
2023-07-28 06:55:48 +0100

    tokenize commands

--- new_strtok.c
+++ new_strtok.c
@@ -1,67 +1,67 @@
 #include "shell.h"
 
 /**
- * check_match - checks if a character matches any in a string
- * @c: character to check
- * @str: string to check
+ * check_match - fxn to check if a char matches any in a string
+ * @c: Given character to check for
+ * @str: the string to check character in
  *
- * Return: 1 if match, 0 if not
+ * Return: returns 1 if checks match, 0 if no match
  */
 unsigned int check_match(char c, const char *str)
 {
-unsigned int i;
+	unsigned int i;
 
-for (i = 0; str[i] != '\0'; i++)
-{
-if (c == str[i])
-return (1);
-}
-return (0);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (c == str[i])
+			return (1);
+	}
+	return (0);
 }
 
 /**
- * new_strtok - custom strtok
- * @str: string to tokenize
- * @delim: delimiter to tokenize against
+ * new_strtok - Our custom strtok funtion for parsing
+ * @str: the string to parse
+ * @delim: the delimiter
  *
- * Return: pointer to the next token or NULL
+ * Return: returns a pointer to the next token or NULL
  */
 char *new_strtok(char *str, const char *delim)
 {
-static char *token_start;
-static char *next_token;
-unsigned int i;
+	static char *token_start;
+	static char *next_token;
+	unsigned int i;
 
-if (str != NULL)
-next_token = str;
-token_start = next_token;
-if (token_start == NULL)
-return (NULL);
-for (i = 0; next_token[i] != '\0'; i++)
-{
-if (check_match(next_token[i], delim) == 0)
-break;
-}
-if (next_token[i] == '\0' || next_token[i] == '#')
-{
-next_token = NULL;
-return (NULL);
-}
-token_start = next_token + i;
-next_token = token_start;
-for (i = 0; next_token[i] != '\0'; i++)
-{
-if (check_match(next_token[i], delim) == 1)
-break;
-}
-if (next_token[i] == '\0')
-next_token = NULL;
-else
-{
-next_token[i] = '\0';
-next_token = next_token + i + 1;
-if (*next_token == '\0')
-next_token = NULL;
-}
-return (token_start);
+	if (str != NULL)
+	next_token = str;
+	token_start = next_token;
+	if (token_start == NULL)
+		return (NULL);
+	for (i = 0; next_token[i] != '\0'; i++)
+	{
+		if (check_match(next_token[i], delim) == 0)
+			break;
+	}
+	if (next_token[i] == '\0' || next_token[i] == '#')
+	{
+		next_token = NULL;
+		return (NULL);
+	}
+	token_start = next_token + i;
+	next_token = token_start;
+	for (i = 0; next_token[i] != '\0'; i++)
+	{
+		if (check_match(next_token[i], delim) == 1)
+			break;
+	}
+	if (next_token[i] == '\0')
+		next_token = NULL;
+	else
+	{
+		next_token[i] = '\0';
+		next_token = next_token + i + 1;
+		if (*next_token == '\0')
+			next_token = NULL;
+	}
+	return (token_start);
 }
